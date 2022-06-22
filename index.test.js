aa580afd58dd15ecabb2cfd80d022a1b43431702
Peter <rai.peter2@gmail.com>
2022-06-22 15:42:04 +0530

    changes new user

--- index.test.js
+++ index.test.js
@@ -11,25 +11,25 @@ describe("User Authentication Test", () => {
         const response = await request("http://front-end:8079")
             .post('/register')
             .send({
-                username: "smarter1",
-                firstName: "codes1",
-                lastName: "smarter1",
-                email: "smarter@codes1.com",
-                password: "smarter1"
+                username: "smarter5",
+                firstName: "codes5",
+                lastName: "smarter5",
+                email: "smarter@codes5.com",
+                password: "smarter5"
             });
         expect(response.status).toBe(200);
     })
     it("GET /login Logs in the registered user", async () => {
         const response = await request("http://front-end:8079")
             .get('/login')
-            .auth('smarter', 'smarter')
+            .auth('user', 'password')
         expect(response.status).toBe(200);
 
     })
     it("GET /login Returns unauthorized error if logged in with wrong credential", async () => {
         const response = await request("http://front-end:8079")
             .get('/login')
-            .auth('smarter', 'smarterasdf')
+            .auth('user', 'wrongpassword')
         expect(response.status).toBe(401);
     })
 })
